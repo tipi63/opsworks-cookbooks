0393727bd9056323ac5d743a57c03b442e3c9e51
Elie SIAUVE <elie@taketracker.com>
2013-08-30 12:36:14 +0200

    ENH: add clean uploads freemium cron

--- phpapi/recipes/cronjobs.rb
+++ phpapi/recipes/cronjobs.rb
@@ -2,3 +2,8 @@ cron "clean_auploader_files" do
   minute "*/5"
   command "/srv/www/taketrackerapi/current/protected/yiic cleanauploderfiles clean"
 end
+cron "clean_uploads_freemium" do
+  minute "0"
+  hour "0"
+  command "/srv/www/taketrackerapi/current/protected/yiic cleanuploadsfreemium clean"
+end
