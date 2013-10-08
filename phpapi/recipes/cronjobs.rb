3872dbb977266d5cad8fd581ba9c8e0bed51fe9f
Elie SIAUVE <elie@taketracker.com>
2013-10-08 22:27:47 +0700

    ENH: add resend to conversion cron

--- phpapi/recipes/cronjobs.rb
+++ phpapi/recipes/cronjobs.rb
@@ -2,6 +2,10 @@ cron "clean_auploader_files" do
   minute "*/5"
   command "/srv/www/taketrackerapi/current/protected/yiic cleanauploderfiles clean"
 end
+cron "resend_to_conversion" do
+  minute "*/5"
+  command "/srv/www/taketrackerapi/current/protected/yiic resendtoconversion send"
+end
 cron "clean_uploads_freemium" do
   minute "0"
   hour "0"
